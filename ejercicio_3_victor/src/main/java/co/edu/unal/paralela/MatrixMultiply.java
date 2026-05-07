20347cfd1a24b159ca764016d751c67a3c9fb0a2
Victor Saa <vis97c@hotmail.com>
2026-05-07 08:32:46 -0500

    parallelization attemp ex3 victor

--- ejercicio_3_victor/src/main/java/co/edu/unal/paralela/MatrixMultiply.java
+++ ejercicio_3_victor/src/main/java/co/edu/unal/paralela/MatrixMultiply.java
@@ -1,9 +1,19 @@
-package co.unal.edu.unal.paralela;
+package co.edu.unal.paralela;
 
 import static edu.rice.pcdp.PCDP.forseq2d;
 
+import java.util.stream.IntStream;
+
+import edu.rice.pcdp.ProcedureInt2D;
+
+import static edu.rice.pcdp.PCDP.async;
+import static edu.rice.pcdp.PCDP.finish;
+import static edu.rice.pcdp.PCDP.forall;
+import static edu.rice.pcdp.PCDP.forall2d;
+
 /**
- * Clase envolvente pata implementar de forma eficiente la multiplicación dde matrices en paralelo.
+ * Clase envolvente pata implementar de forma eficiente la multiplicación dde
+ * matrices en paralelo.
  */
 public final class MatrixMultiply {
     /**
@@ -13,7 +23,8 @@ public final class MatrixMultiply {
     }
 
     /**
-     * Realiza una multiplicación de matrices bidimensionales (A x B = C) de forma secuencial.
+     * Realiza una multiplicación de matrices bidimensionales (A x B = C) de forma
+     * secuencial.
      *
      * @param A Una matriz de entrada con dimensiones NxN
      * @param B Una matriz de entrada con dimensiones NxN
@@ -31,23 +42,36 @@ public final class MatrixMultiply {
     }
 
     /**
-     * Realiza una multiplicación de matrices bidimensionales (A x B = C) de forma paralela.
+     * Realiza una paralelizacion de bucles 2d
+     *
+     * @param N1   Tamaño del bucle externo
+     * @param N2   Tamaño del bucle interno
+     * @param body Cuerpo del bucle
+     */
+    public static void parStreamForAll2d(final int N1, final int N2, final ProcedureInt2D body) {
+        // IntStream.range(0, N1).parallel().forEach(i -> {
+        // IntStream.range(0, N2).parallel().forEach(j -> body.apply(i, j));
+        // });
+
+        forall(0, N1 - 1, i -> forall(0, N2 - 1, j -> body.apply(i, j)));
+    }
+
+    /**
+     * Realiza una multiplicación de matrices bidimensionales (A x B = C) de forma
+     * paralela.
      *
      * @param A Una matriz de entrada con dimensiones NxN
      * @param B Una matriz de entrada con dimensiones NxN
      * @param C Matriz de salida
      * @param N amaño de las matrices de entrada
      */
-    public static void parMatrixMultiply(final double[][] A, final double[][] B,
-            final double[][] C, final int N) {
-        /*
-         * PARA HACER: paralelizar el ciclo externo para mejorar el desempeño.
-         */
-        forseq2d(0, N - 1, 0, N - 1, (i, j) -> {
-            C[i][j] = 0.0;
-            for (int k = 0; k < N; k++) {
-                C[i][j] += A[i][k] * B[k][j];
-            }
+    public static void parMatrixMultiply(double[][] A, double B[][], double[][] C, int n) {
+        parStreamForAll2d(n, n, (i, j) -> {
+            C[i][j] = 0;
+            // for (int k = 0; k < n; k++) {
+            // C[i][j] += A[i][k] * B[k][j];
+            // }
+            forall(0, n - 1, k -> C[i][j] += A[i][k] * B[k][j]);
         });
     }
 }
--- ejercicio_3_victor/src/test/java/co/edu/unal/paralela/MatrixMultiplyTest.java
+++ ejercicio_3_victor/src/test/java/co/edu/unal/paralela/MatrixMultiplyTest.java
@@ -5,7 +5,8 @@ import java.util.Random;
 import junit.framework.TestCase;
 
 public class MatrixMultiplyTest extends TestCase {
-    // Número de veces que se repetirá cada test, para tener resultados de tiempo consistentes.
+    // Número de veces que se repetirá cada test, para tener resultados de tiempo
+    // consistentes.
     final static private int REPEATS = 20;
 
     private static int getNCores() {
@@ -18,7 +19,8 @@ public class MatrixMultiplyTest extends TestCase {
     }
 
     /**
-     * Crea un arreglo double[] de longitud N para utilizar como esntrada para cada test.
+     * Crea un arreglo double[] de longitud N para utilizar como esntrada para cada
+     * test.
      *
      * @param N Tamaño del arreglo a crear
      * @return Arreglo double inicializado de longitud N
@@ -49,7 +51,8 @@ public class MatrixMultiplyTest extends TestCase {
     }
 
     /**
-     * Una implementación de referencia para seqMatrixMultiply, en caso de que algun de los archivos fuentes principales se maodificado accidentalmente.
+     * Una implementación de referencia para seqMatrixMultiply, en caso de que algun
+     * de los archivos fuentes principales se maodificado accidentalmente.
      */
     public void seqMatrixMultiply(final double[][] A, final double[][] B, final double[][] C, final int N) {
         for (int i = 0; i < N; i++) {
@@ -66,7 +69,8 @@ public class MatrixMultiplyTest extends TestCase {
      * Función para apoyar las pruebas de la implementación paralela de dos tareas.
      *
      * @param N El tamaño de los arreglos a evaluar
-     * @return La mejora en la rapidez lograda, No todas pruebas utilizan esta información
+     * @return La mejora en la rapidez lograda, No todas pruebas utilizan esta
+     *         información
      */
     private double parTestHelper(final int N) {
         // Crea una entrada de forma aleatoria
@@ -75,7 +79,8 @@ public class MatrixMultiplyTest extends TestCase {
         final double[][] C = new double[N][N];
         final double[][] refC = new double[N][N];
 
-        // Utiliza una version secuencial de referencia para calcular el resultado correcto
+        // Utiliza una version secuencial de referencia para calcular el resultado
+        // correcto
         seqMatrixMultiply(A, B, refC, N);
 
         // Utiliza la implementación paralela para calcular el resultado
@@ -84,7 +89,8 @@ public class MatrixMultiplyTest extends TestCase {
         checkResult(refC, C, N);
 
         /*
-         * Ejecuta varias repeticiones de las versiones secuencial y paralela para obtener una medición exacta del desempeño en paralelo
+         * Ejecuta varias repeticiones de las versiones secuencial y paralela para
+         * obtener una medición exacta del desempeño en paralelo
          */
         final long seqStartTime = System.currentTimeMillis();
         for (int r = 0; r < REPEATS; r++) {
@@ -101,28 +107,30 @@ public class MatrixMultiplyTest extends TestCase {
         final long seqTime = (seqEndTime - seqStartTime) / REPEATS;
         final long parTime = (parEndTime - parStartTime) / REPEATS;
 
-        return (double)seqTime / (double)parTime;
+        return (double) seqTime / (double) parTime;
     }
 
     /**
-     * Prueba el desempeño de la implementación paralela con una matriz de tamaño 512x512.
+     * Prueba el desempeño de la implementación paralela con una matriz de tamaño
+     * 512x512.
      */
     public void testPar512_x_512() {
         final int ncores = getNCores();
         double speedup = parTestHelper(512);
-        double minimalExpectedSpeedup = (double)ncores * 0.6;
+        double minimalExpectedSpeedup = (double) ncores * 0.6;
         final String errMsg = String.format("It was expected that the parallel implementation would run at " +
                 "least %fx faster, but it only achieved %fx speedup", minimalExpectedSpeedup, speedup);
         assertTrue(errMsg, speedup >= minimalExpectedSpeedup);
     }
 
     /**
-     * Prueba el desempeño de la implementación paralela con una matriz de tamaño 768x768.
+     * Prueba el desempeño de la implementación paralela con una matriz de tamaño
+     * 768x768.
      */
     public void testPar768_x_768() {
         final int ncores = getNCores();
         double speedup = parTestHelper(768);
-        double minimalExpectedSpeedup = (double)ncores * 0.6;
+        double minimalExpectedSpeedup = (double) ncores * 0.6;
         final String errMsg = String.format("It was expected that the parallel implementation would run at " +
                 "least %fx faster, but it only achieved %fx speedup", minimalExpectedSpeedup, speedup);
         assertTrue(errMsg, speedup >= minimalExpectedSpeedup);
